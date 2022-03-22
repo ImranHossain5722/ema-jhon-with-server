59855aeb4a5d89a68ccb355585c36374e1f583a7
ImranHossain5722 <imd572258@gmail.com>
2022-03-22 22:20:32 +0600

    this a shopping shoping page use react framwork

---
+++ src/components/Product/Product.js
@@ -0,0 +1,27 @@
+import React from 'react';
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+import './Product.css'
+
+const Product = (props) => {
+    const { name, img, seller, price, ratings} =props.product
+    return (
+        <div className='product' >
+        <img src={img} alt="" />
+      <div className="product-info">
+        <p className='Product-Name' >{name}</p>
+        <p>Price: ${price}</p>
+        <p>Seller: {seller}</p>
+        <p>Ratings: {ratings} stars</p>        </div>
+            <button onClick={ ()=> props.handelToCart(props.product)} className='button-cart'>
+                <p className='btn-cart'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}> 
+
+                </FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    const [products,setProducts] = useState([])
+    const [cart, setcart]= useState([])
+
+    useEffect(()=>{
+        fetch ('products.json')
+        .then(res=> res.json() )
+        .then(data => setProducts(data))
+
+    },[] )
+
+    const handelToCart =(product)=>{
+        console.log(product);
+        const newCart =[...cart,product]
+        setcart(newCart)
+    }
+    return (
+        <div className='shop-container'>
+                <div className="products">
+                    {
+                        products.map(product => <Product key={product.id}
+                         
+                            product={product}
+                            handelToCart ={handelToCart}
+                         
+                         ></Product> )
+                    }
+
+                </div>
+                <div className="orderSummery">
+                    <h4>this for orderSummery</h4>
+                    <p> items:{cart.length}</p>
+
+                </div>
+        </div>
+    );
+};
+
+export default Shop;
