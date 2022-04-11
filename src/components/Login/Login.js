8f6af63887187708e6aa95093f0bf526093e5e0c
ImranHossain5722 <imd572258@gmail.com>
2022-04-11 12:35:08 +0600

    Create Require Auth and Navigate from

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState}  from 'react';
+import { useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('')
+
+    const [SignInWithEmailAndPassword, user, loading, error,] = useSignInWithEmailAndPassword(auth);
+
+  const navigate = useNavigate('')
+
+    const handelEmailBlur =event =>{
+        setEmail(event.target.value)
+    }
+
+    const handelPasswordBlur = event =>{
+        setPassword(event.target.value)
+    }
+
+    if(user){
+        navigate('/shop')
+    }
+    const handelLoginSubmit =event =>{
+        event.preventDefault();
+        SignInWithEmailAndPassword (email,password)
+
+    }
   return (
     <div className="from-container">
       <div className="div">
         <h2 className="loginTitle">Login</h2>
-        <form>
+        <form onSubmit={handelLoginSubmit} >
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" required />
+            <input onBlur= {handelEmailBlur} type="email" required />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" required />
+            <input onBlur={handelPasswordBlur} type="password" name="password" required />
           </div>
+          <p style={{color:'red'}}>{error?.message}</p>
+          {
+              loading && <p>Loading..</p>
+          }
           <input className="submit-btn" type="submit" value="Login" />
         </form>
         <p className="form-bottom-text">
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,28 +1,65 @@
 import React, {useState}  from 'react';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate} from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+
 
 const SignUp = () => {
     const [email,setEmail]=useState('');
     const [password,setPassword]=useState('');
     const [confirmPassword,setConfirmPassword]=useState('');
     const [error,setError]=useState('');
+    const navigate =useNavigate('')
+    const [createUserWithEmailAndPassword, user ]=useCreateUserWithEmailAndPassword(auth)
+
+    const handelEmailBlur =(event)=>{
+            setEmail(event.target.value)
+    }
+
+    const handelPasswordBlur = event =>{
+        setPassword(event.target.value)
+    }
+
+    const handelConfirmPasswordBlur = event =>{
+        setConfirmPassword(event.target.value)
+    }
+        if (user){
+            navigate('/orders')
+        }
+    const handelCreateUser = event =>{
+        event.preventDefault();
+        if (password !== confirmPassword){
+            setError('your two password did not match')
+            return
+        }
+        if (password.length<6){
+            setError('password must be 6 characters or longer')
+            return
+        }
+
+        createUserWithEmailAndPassword(email, password)
+    }
+      
     return (
         <div className="from-container">
         <div className="div">
           <h2 className="loginTitle">Sign Up</h2>
-          <form>
+          <form onSubmit={handelCreateUser} >
             <div className="input-group">
               <label htmlFor="email">Email</label>
-              <input type="email" required />
+              <input onBlur={handelEmailBlur} type="email" required />
             </div>
             <div className="input-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" required />
+              <input onBlur={handelPasswordBlur} type="password" name="password" required />
             </div>
             <div className="input-group">
               <label htmlFor="confirm-password">Confirm Password</label>
-              <input type="password" name="confirm-password" required />
+              <input onBlur={handelConfirmPasswordBlur} type="password" name="confirm-password" required />
             </div>
+            <p style={{color:'red'}}> {error}</p>
             <input className="submit-btn" type="submit" value="Sign Up" />
           </form>
           <p className="form-bottom-text">
